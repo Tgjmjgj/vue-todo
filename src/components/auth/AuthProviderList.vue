50729670b22cf316427ca3ffd2f61bc2a5592a13
Pavel Karpovich <Chesh397@mail.ru>
2019-11-11 02:02:08 +0300

    Login providers buttons

---
+++ src/components/auth/AuthProviderList.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container fluid>
+    <v-row cols="12">
+      <v-btn block
+        color="#d7433b"
+        class="white--text btn"
+        :class="btnClasses"
+      >
+        <div class="btn-cont">
+          <v-icon left style="font-size: 26px">mdi-mail</v-icon>
+          <span style="text-transform: none">
+            Sign in with email
+          </span>
+        </div>
+      </v-btn>
+    </v-row>
+    <v-row cols="12">
+      <v-btn block
+        color="#333"
+        class="white--text btn"
+        :class="btnClasses"
+      >
+        <div class="btn-cont">
+          <v-icon left style="font-size: 26px">mdi-github-box</v-icon>
+          <span style="text-transform: none">
+            Sign in with GitHub
+          </span>
+        </div>
+      </v-btn>
+    </v-row>
+    <v-row cols="12">
+      <v-btn block
+        color="white"
+        class="btn"
+        :class="btnClasses"
+        style="color: #333"
+      >
+        <div class="btn-cont">
+          <v-icon left style="font-size: 26px">mdi-google-plus</v-icon>
+          <span style="text-transform: none">
+            Sign in with Google
+          </span>
+        </div>
+      </v-btn>
+    </v-row>
+    <v-row cols="12">
+      <v-btn block
+        color="#3e5995"
+        class="white--text btn"
+        :class="btnClasses"
+      >
+        <div class="btn-cont">
+          <v-icon left style="font-size: 26px">mdi-facebook-box</v-icon>
+          <span style="text-transform: none">
+            Sign in with Facebook
+          </span>
+        </div>
+      </v-btn>
+    </v-row>
+    <v-row cols="12">
+      <v-btn block
+        color="#5daceb"
+        class="white--text btn"
+        :class="btnClasses"
+      >
+        <div class="btn-cont">
+          <v-icon left style="font-size: 26px">mdi-twitter-box</v-icon>
+          <span style="text-transform: none">
+            Sign in with Twitter
+          </span>
+        </div>
+      </v-btn>
+    </v-row>
+    <v-row cols="12">
+      <v-btn block
+        color="#f1b42f"
+        class="white--text btn"
+        :class="btnClasses"
+      >
+        <div class="btn-cont">
+          <v-icon left style="font-size: 26px">mdi-account-box</v-icon>
+          <span style="text-transform: none">
+            Continue as guest
+          </span>
+        </div>
+      </v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'auth-provider-list',
+  props: {
+    btnClasses: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  box-sizing: border-box;
+  width: 200px;
+  display: flex;
+  justify-content: flex-start;
+  padding-left: 10px;
+  height: 42px !important;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="box fill-height" style="overflow: hidden">
+  <v-container class="box fill-height">
     <v-row justify="center" align="center" class="my-10">
       <v-col cols="12">
         <std-frame>
@@ -17,7 +17,10 @@
               </v-row>
               <v-row>
                 <v-col class="d-flex justify-center">
-                  <div id="firebase-auth"></div>
+                  <auth-provider-list
+                    class="auth-btns"
+                    :btnClasses="['mb-3']"
+                  ></auth-provider-list>
                 </v-col>
               </v-row>
             </v-container>
@@ -29,65 +32,14 @@
 </template>
 
 <script>
-import * as Firebase from 'firebase/app';
-import * as firebaseui from 'firebaseui';
-import 'firebaseui/dist/firebaseui.css';
 import StdFrame from '@/components/StdFrame.vue';
-
-const ui = new firebaseui.auth.AuthUI(Firebase.auth());
+import AuthProviderList from '@/components/auth/AuthProviderList.vue';
 
 export default {
   name: 'login',
   components: {
     'std-frame': StdFrame,
-  },
-  mounted() {
-    ui.start('#firebase-auth', {
-      callbacks: {
-        signInSuccessWithAuthResult(authResult, redirectUrl) {
-          console.log('Successful login!');
-          console.log(authResult);
-          console.log(redirectUrl);
-          return true;
-        },
-        uiShown() {
-          console.log('Shown');
-          // The widget is rendered.
-          // Hide the loader.
-        },
-      },
-      signInFlow: 'popup',
-      signInSuccessUrl: '/#/page/1',
-      signInOptions: [
-        {
-          provider: Firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          requireDisplayName: false,
-        },
-        Firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        {
-          provider: Firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          scopes: [
-            'https://www.googleapis.com/auth/contacts.readonly',
-          ],
-          customParameters: {
-            prompt: 'select_account',
-          },
-        },
-        {
-          provider: Firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          scopes: [
-            'public_profile',
-            'email',
-            'user_likes',
-            'user_friends',
-          ],
-          customParameters: {
-            auth_type: 'reauthenticate',
-          },
-        },
-        Firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      ],
-    });
+    'auth-provider-list': AuthProviderList,
   },
 };
 </script>
@@ -95,13 +47,6 @@ export default {
 <style lang="scss" scoped>
 .box {
   width: auto;
-}
-.body {
-  margin: 0 -12px -12px -12px;
-  background: white;
-}
-#firebase-auth {
-  width: 220px;
 }
 @media (min-width: 400px) {
   .box {
@@ -111,14 +56,7 @@ export default {
 .no-select {
   user-select: none;
 }
-</style>
-
-<style lang="scss">
-.box ul {
-  padding: 0;
-  margin: 0;
-}
-.firebaseui-card-content {
-  padding: 0;
+.auth-btns {
+  width: 230px;
 }
 </style>
