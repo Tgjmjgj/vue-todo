9669f0d5a4856c0f0f9cf918c0cd0efb78fdcd48
Pavel Karpovich <Chesh397@mail.ru>
2019-11-11 03:04:43 +0300

    Remove old unused deps

--- src/main.js
+++ src/main.js
@@ -1,27 +1,11 @@
 import Vue from 'vue';
 import '@/config/firebase';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faUserSecret,
-  faPen,
-  faTimes,
-  faCheck,
-} from '@fortawesome/free-solid-svg-icons';
-import { faClock } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import VuePaginate from 'vue-paginate';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-library.add(faUserSecret, faTimes, faPen, faCheck, faClock);
-
-Vue.use(VuePaginate);
-
 Vue.config.productionTip = false;
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
   router,
